fb191bc10db911a4f6585452898c882b723f3ad6
Erik Ruhmanis <erik2310@edu.easj.dk>
2018-04-27 20:49:56 +0200

    Nu virker den med synchronization.

--- src/SychronizeThreads.java
+++ src/SychronizeThreads.java
@@ -2,15 +2,14 @@ import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 
 public class SychronizeThreads {
-
-    static Integer sum = 0;
+    private static Account account = new Account();
 
     public static void main(String[] args) {
+
         ExecutorService executor = Executors.newCachedThreadPool();
 
         for (int i = 0; i < 1000; i++) {
             executor.execute(new AddOne());
-            System.out.println(sum);
         }
 
         executor.shutdown();
@@ -18,22 +17,35 @@ public class SychronizeThreads {
         while (!executor.isTerminated()) {
         }
 
-        System.out.println(sum);
+        System.out.println(account.sum);
     }
 
     private static class AddOne implements Runnable {
 
         @Override
         public void run() {
-            synchronized (this) {
-                sum = sum + 1;
-                try {
-                    Thread.sleep(5);
-                } catch (InterruptedException ex) {
 
-                }
+            synchronized (account) {
+                account.deposit(1);
             }
+
+        }
+        }
+
+    private static class Account {
+
+        private Integer sum = 0;
+
+        private void deposit(Integer sum) {
+            Integer newSum = this.sum + sum;
+
+            try {
+                Thread.sleep(5);
+            } catch (InterruptedException ex) {
+                ex.printStackTrace();
+            }
+
+            this.sum = newSum;
         }
     }
-
-}
+    }
